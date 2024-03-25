923659b8ccd171eb80c800754e333b092d6ee188
Rafael Avilar <rfl.avilar@outlook.com>
2024-03-24 22:37:22 -0300

    adds t4 and main

---
+++ tests/t4.py
@@ -0,0 +1,17 @@
+from do_test import do_test, do_test_void
+
+# complete as classes abaixo
+# substitua "raise NotImplementedError" pelo seu código
+
+
+class TPessoa:
+    """
+    Crie uma classe Pessoa com os atributos nome e idade.
+    Crie um método aniversario() que incrementa a idade da pessoa em 1.
+    """
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def aniversario(self):
+        raise NotImplementedError

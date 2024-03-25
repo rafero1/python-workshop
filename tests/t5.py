be550e67142f4094f2f4bdcb8d598a91fe8a2cb7
Rafael Avilar <rfl.avilar@outlook.com>
2024-03-24 22:41:24 -0300

    adds t5

---
+++ tests/t5.py
@@ -0,0 +1,19 @@
+from do_test import do_test, do_test_void
+
+# complete as funções abaixo
+# substitua "raise NotImplementedError" pelo seu código
+
+
+def t_escrever_log(filename, texto):
+    """
+    Escreva um texto em um arquivo de log.
+    O arquivo deve ser aberto no modo de adição (append).
+    """
+    raise NotImplementedError
+
+
+def t_ler_log(filename):
+    """
+    Leia o conteúdo de um arquivo de log e retorne como uma string.
+    """
+    raise NotImplementedError
